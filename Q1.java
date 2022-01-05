7651f55efd1285a070ed7b3966f1e63e705b9a70
Shashank Tiwari <67553585+sanktiwarirobotics@users.noreply.github.com>
2022-01-05 19:37:37 +0530

    Create Q1.java

---
+++ Q1.java
@@ -0,0 +1,17 @@
+//Positive or Negative number
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int a=-23;
+        if(a>=0){
+            
+        
+        System.out.println("postive"); 
+    }
+    else{
+        System.out.println("NEGATIVE");
+    }
+    }
+}
