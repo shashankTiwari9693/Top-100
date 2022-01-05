b675a107e08ac2af96027a8b9d15c828f823c717
Shashank Tiwari <67553585+sanktiwarirobotics@users.noreply.github.com>
2022-01-05 19:52:16 +0530

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,17 @@
+//Greatest of two numbers
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+        if(a>b){
+            System.out.println(a+"a is greather than"+b);
+    }
+        else{
+            System.out.println(b+"a is greather than"+a);
+        }
+    }
+}
