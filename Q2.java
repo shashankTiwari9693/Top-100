bc516560cdf1d5ea7142c7b58988722d060476a8
Shashank Tiwari <67553585+sanktiwarirobotics@users.noreply.github.com>
2022-01-05 19:39:16 +0530

    Create Q2.java

---
+++ Q2.java
@@ -0,0 +1,17 @@
+//Even or Odd number
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int a=22;
+        if(a%2!=0){
+            
+        
+        System.out.println("odd"); 
+    }
+    else{
+        System.out.println("Even");
+    }
+    }
+}
